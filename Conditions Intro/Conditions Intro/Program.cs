ef33c19ab3ea64ec8d3eca436b1ea0e58dcf14a4
SanderK36 <sander.k606@gmail.com>
2025-05-07 00:20:29 +0200

    07.05

---
+++ Conditions Intro/Conditions Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Conditions_Intro
+{
+    internal class Program
+    {
+        int number = 5;
+        int number2 = 3;
+        public void Run()
+        {
+            
+        }
+    }
+}
