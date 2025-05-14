eee8027428d12eb6a523ca407c63cc5474ed4620
SanderK36 <sander.k606@gmail.com>
2025-05-14 14:20:45 +0200

    14.05

---
+++ ParProgrammering/ParProgrammering/Ole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProgrammering
+{
+    public class Ole()
+    {
+        private int Age = 28;
+        private string Name = "Ole";
+        private string Hobby = "Trening-og-tur";
+        private string FavMusic = "Hardstyle-og-Metal";
+
+        public void run()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"Hallo! Jeg heter {Name} og er {Age} år gammel :)");
+            Console.WriteLine($"Mine hobbyer er å {Hobby}");
+            Console.WriteLine($"Min favortitt musikk er {FavMusic} ");
+            Console.WriteLine("----------------------------------------------------");
+        }
+    }
+
+
+}
---
+++ ParProgrammering/ParProgrammering/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ParProgrammering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Sander", 24, "spille gitar", "Metal"),
+                new Person("Ole", 28, "Trening-og-tur", "Hardstyle-og-Metal")
+            };
+            foreach (Person person in people)
+            {
+                person.Run();
+            }
+        }
+    }
+}
